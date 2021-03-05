f2ae728b99a6f3d51f11dd0c430ac40bc627769c
habib houideg <houideghabib@gmail.com>
2021-03-05 13:39:53 +0100

    maaj habib

--- src/Pages/Home-habib.js
+++ src/Pages/Home-habib.js
@@ -6,25 +6,27 @@ import '../Style/Home-habib.css';
 import News from '../Componant/News'
 import Menu from '../Componant/Menu';
 
-function Home  (){
-  
-    return (
-      <div className="body logo" >
-        <Menu/>
-        <form>
-          <input type="button" value="Démarrer la machine"/>
-        </form>
-        <div className="text capitale ">
+function Home() {
+
+  return (
+    <>
+      <Menu />
+
+      <div className="logo" >
+
+        <input type="button" value="Créer son compte" />
+        <div className= "titre">
           <h1>Bienvenue sur Whats game</h1>
-          <h2>La première plateforme française</h2>     
+          <h2>La première plateforme française</h2>
           <h2>de statistiques, de recrutement et</h2>
           <h2>d'actualités autour de l'esport.</h2>
 
-     </div>
+        </div>
 
       </div>
-    );
-  
+    </>
+  );
+
 }
 
 
