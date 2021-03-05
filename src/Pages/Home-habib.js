70dadc91ad30c2fd5539aa36d9b0681fd97c19a9
habib houideg <houideghabib@gmail.com>
2021-03-05 12:12:35 +0100

    MODIF CSS HABIB

--- src/Pages/Home-habib.js
+++ src/Pages/Home-habib.js
@@ -4,17 +4,22 @@ import {
 } from "react-router-dom";
 import '../Style/Home-habib.css';
 import News from '../Componant/News'
+import Menu from '../Componant/Menu';
 
 function Home  (){
   
     return (
       <div className="body logo" >
-        
+        <Menu/>
         <form>
-  <input type="button" value="Démarrer la machine"/>
-</form>
-<div className="text capitale ">
-    <h1 Bienvenue sur What s game></h1>
+          <input type="button" value="Démarrer la machine"/>
+        </form>
+        <div className="text capitale ">
+          <h1>Bienvenue sur Whats game</h1>
+          <h2>La première plateforme française</h2>     
+          <h2>de statistiques, de recrutement et</h2>
+          <h2>d'actualités autour de l'esport.</h2>
+
      </div>
 
       </div>
