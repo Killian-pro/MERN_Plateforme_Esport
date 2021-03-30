1be905b5d351b69ac26ac68834c344e8472f2362
habib houideg <houideghabib@gmail.com>
2021-03-30 09:40:15 +0200

    bouton page accueil

--- src/Pages/Home-habib.js
+++ src/Pages/Home-habib.js
@@ -16,10 +16,16 @@ function Home() {
         <input type="button" value="Créer son compte" />
         <div className= "titre">
           <h1>Bienvenue sur Whats game</h1>
+        <div className="soustitre">
           <h2>La première plateforme française</h2>
           <h2>de statistiques, de recrutement et</h2>
           <h2>d'actualités autour de l'esport.</h2>
-
+          </div>
+          <div class="buttonconnexion">
+            <button type="button" class="btn">Créer son compte</button>
+          </div>
+            <canvas id="canvas"></canvas>
+        
         </div>
 
       </div>
