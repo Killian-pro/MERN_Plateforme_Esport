a0d52f34a00e3d12c2414cff0b2424686b88b377
kip <cluzel.killian@gmail.com>
2021-03-31 11:39:20 +0200

    Modif pour Habib

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-import {
-  Link
-} from "react-router-dom";
-import '../Style/Home-habib.css';
+import React from 'react';
+import '../Style/Home.css';
 import Menu from '../Component/Menu';
 
 function Home() {
@@ -10,23 +7,16 @@ function Home() {
   return (
     <>
       <Menu />
-
       <div className="logo" >
-
-        <div className= "titre">
+        <div className="titre">
           <h1>Bienvenue sur Whats game</h1>
-        <div className="soustitre">
-          <h2>La première plateforme française</h2>
-          <h2>de statistiques, de recrutement et</h2>
-          <h2>d'actualités autour de l'esport.</h2>
+          <div className="soustitre">
+            <h2>La première plateforme française</h2>
+            <h2>de statistiques, de recrutement et</h2>
+            <h2>d'actualités autour de l'esport.</h2>
           </div>
-          <div class="buttonconnexion">
-            <button type="button" class="btn">Créer son compte</button>
-          </div>
-            <canvas id="canvas"></canvas>
-        
+          <button type="button" class="btn">Créer son compte</button>
         </div>
-
       </div>
     </>
   );
