eea466f8f4ce6d7cb88a4f13cf8f70fae6dd9cc9
habib houideg <houideghabib@gmail.com>
2021-05-28 11:10:01 +0200

    modif page d'accueil

--- Front/src/Pages/Home.js
+++ Front/src/Pages/Home.js
@@ -5,6 +5,11 @@ import statimg from '../Img/statistics.png';
 import equipelogo from '../Img/deal.png';
 import twitchlogo from '../Img/twitch.png';
 import Footer from '../Component/Footer';
+import News from '../Component/News/MainNews';
+import setup from '../Img/setup.jpg';
+import {
+  Link
+} from "react-router-dom";
 function Home() {
 
   return (
@@ -32,15 +37,15 @@ function Home() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Duis vel felis at metus sodales cursus et eget sapien.
             Proin iaculis, sapien et faucibus volutpat, nisl augue mattis lacus,
-             id egestas sapien velit eu lectus. Praesent ac sem lectus. Cras vel mollis lacus.
+            id egestas sapien velit eu lectus. Praesent ac sem lectus. Cras vel mollis lacus.
           </div>
           <div className="end">
-          <button type="button" class="btnplus">Voir plus</button>
+            <button type="button" class="btnplus">Voir plus</button>
           </div>
         </div>
       </div>
       <div className="blocrecrutement">
-        
+
         <div className="soustitress">
           <div className="title">Rechercher grâce à notre outil des joueurs et des équipes</div>
           <div className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -48,16 +53,15 @@ function Home() {
           Proin iaculis, sapien et faucibus volutpat, nisl augue mattis lacus,
           id egestas sapien velit eu lectus. Praesent ac sem lectus. Cras vel mollis lacus.</div>
           <div className="start">
-        <button type="button" class="btnplus">Voir plus</button>
-        </div>
+            <button type="button" class="btnplus">Voir plus</button>
+          </div>
         </div>
         <div className="recrutlogo"><img className="equipelogo" src={equipelogo} alt="Logo" /></div>
-       
       </div>
       <div className="twitchbloc">
         <div className="tlogo"><img className="twitchlogo" src={twitchlogo} alt="Logo" /></div>
         <div className="soustitres">
-          <div className="title debut"> 
+          <div className="title debut">
             Consulter les statistiques en temps
             réel sur tous les jeux du moment
           </div>
@@ -68,12 +72,37 @@ function Home() {
             id egestas sapien velit eu lectus. Praesent ac sem lectus. Cras vel mollis lacus.
           </div>
           <div className="end">
-          <button type="button" class="btnplus">Voir plus</button>
+            <button type="button" class="btnplus">Voir plus</button>
           </div>
+        </div>
       </div>
-      
+      <div style={{alignContent:'center',backgroundColor: '#F5F5F5',padding:'3%'}}>
+        <h1 style={{textAlign:'center', paddingBottom:'3%',color:'#645D5D'}}>
+          Nos dernières actualités
+        </h1>
+        <div style={{ flexdirection: 'row', display: 'flex'}}>
+          <div >
+            <img style={{ width: '55%', borderRadius: 10 }} src={setup} alt="article" />
+            <News id={1} description={false} />
+          </div>
+          <div >
+            <img style={{ width: '55%', borderRadius: 10 }} src={setup} alt="article" />
+            <News id={2} description={false} />
+          </div>
+          <div >
+            <img style={{ width: '55%', borderRadius: 10 }} src={setup} alt="article" />
+            <News id={3} description={false} />
+          </div>
+          <div >
+            <img style={{ width: '55%', borderRadius: 10 }} src={setup} alt="article" />
+            <News id={4} description={false} />
+          </div>
+        </div>
+        <div style={{textAlign:'center',padding:'2%',fontSize:'130%'}}>
+        <Link style={{ color: '#827878', textDecoration: 'none', fontWeight: 'bold', fontSize: '75%',alignItems:'center',color:'#440BD4'}} to="/Actualites">Voir plus d'articles dans notre page dédiée</Link> 
+        </div>   
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 
