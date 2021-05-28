2e18df3b01e6075d39e1f9eb27eebd5314162732
habib houideg <houideghabib@gmail.com>
2021-05-28 10:15:28 +0200

    page accueil modif habib

--- Front/src/Pages/Home.js
+++ Front/src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../Style/Home.css';
 import Menu from '../Component/Menu';
-
+import statimg from '../Img/statistics.png';
+import equipelogo from '../Img/deal.png';
+import twitchlogo from '../Img/twitch.png';
 function Home() {
 
   return (
-    <>
+    <div>
       <Menu />
       <div className="logo" >
         <div className="titre">
@@ -18,7 +20,59 @@ function Home() {
           <button type="button" class="btn">Créer son compte</button>
         </div>
       </div>
-    </>
+      <div className="statbloc">
+        <div className="statlogo"><img className="statimg" src={statimg} alt="Logo" /></div>
+        <div className="soustitres">
+          <div className="title">
+            Consulter les statistiques en temps
+            réel sur tous les jeux du moment
+          </div>
+          <div className="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Duis vel felis at metus sodales cursus et eget sapien.
+            Proin iaculis, sapien et faucibus volutpat, nisl augue mattis lacus,
+             id egestas sapien velit eu lectus. Praesent ac sem lectus. Cras vel mollis lacus.
+          </div>
+          <div className="end">
+          <button type="button" class="btnplus">Voir plus</button>
+          </div>
+        </div>
+      </div>
+      <div className="blocrecrutement">
+        
+        <div className="soustitress">
+          <div className="title">Rechercher grâce à notre outil des joueurs et des équipes</div>
+          <div className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Duis vel felis at metus sodales cursus et eget sapien.
+          Proin iaculis, sapien et faucibus volutpat, nisl augue mattis lacus,
+          id egestas sapien velit eu lectus. Praesent ac sem lectus. Cras vel mollis lacus.</div>
+          <div className="start">
+        <button type="button" class="btnplus">Voir plus</button>
+        </div>
+        </div>
+        <div className="recrutlogo"><img className="equipelogo" src={equipelogo} alt="Logo" /></div>
+       
+      </div>
+      <div className="twitchbloc">
+        <div className="tlogo"><img className="twitchlogo" src={twitchlogo} alt="Logo" /></div>
+        <div className="soustitres">
+          <div className="title debut"> 
+            Consulter les statistiques en temps
+            réel sur tous les jeux du moment
+          </div>
+          <div className="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Duis vel felis at metus sodales cursus et eget sapien.
+            Proin iaculis, sapien et faucibus volutpat, nisl augue mattis lacus,
+            id egestas sapien velit eu lectus. Praesent ac sem lectus. Cras vel mollis lacus.
+          </div>
+          <div className="end">
+          <button type="button" class="btnplus">Voir plus</button>
+          </div>
+      </div>
+      
+      </div>
+    </div>
   );
 
 }
